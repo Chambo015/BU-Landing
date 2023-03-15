bceba63be2b8ae5ef895d4f58fef227f43d48564
Eraly <thegunner125@gmail.com>
2023-03-15 13:39:28 +0600

    feat: animation

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -7,13 +7,18 @@ import TheInfiniteTicker from './TheInfiniteTicker.vue';
 
 <template>
     <div class="bg-[#181818]">
-        <header class="container mb-24 pt-[60px]">
+        <header class="container mb-24 pt-[60px] " v-motion="{ 
+    duration: 800, // duration in milliseconds
+    initial: { opacity: 0 , y: 100},
+    visibleOnce: { opacity: 1 , y: 0}
+  }">
             <IconLogo />
         </header>
-        <section class="container pb-32 flex justify-between">
-            <div class="w-[830px]">
-                <h1 class="mb-12 text-6xl font-bold leading-tight">
-                    <span class="text-[#30A2FF]">Блокчейн</span> разработка на самом востребованном языке программирования
+        <section class="container flex justify-between pb-32">
+            <div v-motion-slide-visible-once-left class="w-[830px]">
+                <h1  class="mb-12 text-6xl font-bold leading-tight">
+                    <span class="text-[#30A2FF]">Блокчейн</span> разработка на самом востребованном языке
+                    программирования
                 </h1>
                 <p class="mb-28 text-2xl font-bold leading-tight">С нуля до уровня Junior+</p>
                 <div class="mb-28">
@@ -34,17 +39,36 @@ import TheInfiniteTicker from './TheInfiniteTicker.vue';
                     </div>
                 </div>
             </div>
-            <div>
+            <div
+                v-motion
+                :initial="{
+                    opacity: 0,
+                    y: 200,
+                }"
+                :visibleOnce="{
+                    opacity: 1,
+                    y: 0,
+                }"
+                >
                 <img :src="blockchainImg" alt="super img" width="700" height="700" class="-translate-y-8 -rotate-12" />
             </div>
         </section>
     </div>
     <TheInfiniteTicker />
     <section class="container py-20">
-        <ul class="list-disc text-5xl font-medium flex justify-between ml-10">
-            <li>Обучение<p class="text-3xl font-normal"> на реальных кейсах</p></li>
-            <li>Онлайн<p class="text-3xl font-normal"> в удобное время</p></li>
-            <li>Помощь<p class="text-3xl font-normal"> в трудоустройстве</p></li>
+        <ul class="ml-10 flex list-disc justify-between text-5xl font-medium">
+            <li v-motion-roll-visible-once-left>
+                Обучение
+                <p class="text-3xl font-normal">на реальных кейсах</p>
+            </li>
+            <li v-motion-roll-visible-once-left>
+                Онлайн
+                <p class="text-3xl font-normal">в удобное время</p>
+            </li>
+            <li v-motion-roll-visible-once-left>
+                Помощь
+                <p class="text-3xl font-normal">в трудоустройстве</p>
+            </li>
         </ul>
     </section>
 </template>
