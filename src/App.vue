82140532721ec457b35b66542250574c87be3369
Eraly <thegunner125@gmail.com>
2023-05-05 17:24:57 +0600

    feat: alert workshop

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {onMounted, provide} from 'vue';
+import {onMounted, onUnmounted, provide, ref} from 'vue';
 import TheCTA from './components/TheCTA.vue';
 import TheCases from './components/TheCases.vue';
 import TheCertificates from './components/TheCertificates.vue';
@@ -16,6 +16,7 @@ import TheTeam from './components/TheTeam.vue';
 // import TheWorkShops from './components/TheWorkShops.vue';
 import {animate, inView} from 'motion';
 import {useMediaQuery} from '@vueuse/core';
+import WorkShopAlert from './components/WorkShopAlert.vue';
 
 const isLargerSm = useMediaQuery('(min-width: 640px)');
 const launchAnimations = () => {
@@ -33,13 +34,34 @@ const launchAnimations = () => {
 
 onMounted(() => {
   isLargerSm.value && launchAnimations()
+  
+  setTimeout(() => {
+    visibleModalWorkShop.value = true
+  }, 3000)
+
+  document.body.addEventListener('keydown', handleEscapeDown)
 })
+
+onUnmounted(() => {
+  document.body.removeEventListener('keydown', handleEscapeDown)
+})
+
+const visibleModalWorkShop = ref(false);
+const closeModalWorkshop = () => {
+  visibleModalWorkShop.value = false
+}
+
+const handleEscapeDown = (e) => {
+    if( e.key === 'Escape' && visibleModalWorkShop) {
+      closeModalWorkshop()
+    } 
+}
 
 provide('isLargerSm', isLargerSm)
 </script>
 
 <template>
-  <main v-cloak>
+  <main v-cloak >
     <TheHero />
     <TheCases />
     <TheSkillsDev />
@@ -55,8 +77,17 @@ provide('isLargerSm', isLargerSm)
     <TheFooter />
   </main>
   <TheChat />
+  <Transition name="modal"><WorkShopAlert  v-if="visibleModalWorkShop" :close-modal="closeModalWorkshop" /></Transition>
 </template>
 
-<style>
+<style scoped >
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.5s ease;
+}
 
+.modal-enter-from,
+.modal-leave-to {
+  opacity: 0;
+}
 </style>
---
+++ src/components/WorkShopAlert.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="fixed w-full h-full inset-x-0 inset-y-0 z-50 bg-black/40 backdrop-blur-sm">
+        <div
+            class="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-50 w-[650px] 2xl:w-[750px] rounded-md border border-white/25 bg-[rgba(20,21,20,.9)] 2xl:p-8  p-5 shadow-2xl backdrop-blur-sm">
+            <div>
+                <p class="text-center text-3xl font-bold uppercase 2xl:text-5xl">
+                    <span class="text-[rgba(125,198,137,1)]">Бесплатный</span> Воркшоп
+                </p>
+                <ul class="mt-5 font-['TT_Octosquares'] text-lg [&_span]:font-semibold [&_span]:text-[rgba(125,198,137,1)]">
+                    <li><span>Тема:</span> Блокчейн разработчик</li>
+                    <li><span>Выступает:</span> основатели BU + старший сеньор разработчик Кирилл Зайнутдинов</li>
+                    <li><span>Когда:</span> в эту субботу в 16:00, в zoom конференции</li>
+                </ul>
+                <div  v-if="!isOpenForm" class="flex justify-center">
+                    <button
+                       
+                        @click="isOpenForm = true"
+                        class="mt-5 rounded bg-[rgba(103,176,115,1)] px-8 py-2 text-center text-xl hover:bg-[#5a8f63]">
+                        Записаться
+                    </button>
+                    
+                </div>
+                <form @submit="onSubmitHandler" v-else class="mt-5">
+                        <input
+                            v-model="form.name"
+                            required
+                            minlength="3"
+                            type="text"
+                            placeholder="Имя"
+                            class="mb-5 w-full rounded bg-[#2b2b2b3b] py-3 px-4 text-base 2xl:text-xl" />
+                        <p class="text-sm text-white/50">*Укажите WhatsApp номер, чтобы получить ссылку на Воркшоп</p>
+                        <input
+                            pattern="[+]\d-\(\d{3}\)-\d{3}-\d{2}-\d{2}"
+                            v-model="form.phone"
+                            required
+                            v-maska="maskBinded"
+                            data-maska="+7-(###)-###-##-##"
+                            title="+7-(7XX)-XXX-XX-XX"
+                            type="phone"
+                            placeholder="Номер телефона"
+                            class="mb-5 w-full rounded bg-[#2b2b2b3b] py-3 px-4 text-base 2xl:mb-7 2xl:text-xl" />
+                        <button class="rounded-sm hover:bg-[#7cd88b] bg-[rgba(103,176,115,1)] px-6 py-2 2xl:text-xl">Записаться</button>
+                    </form>
+            </div>
+    
+    
+            <button
+                @click="closeModal"
+                class="absolute right-0 top-0 h-7 w-7 translate-x-1/2 -translate-y-1/2 rounded-md border border-white/25 bg-[#1c1d1c]">
+                x
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+import { vMaska } from 'maska';
+import { getDatabase, ref as refFirebase, set, push } from 'firebase/database';
+
+const props = defineProps({
+    closeModal: Function
+})
+
+const result = ref(false);
+const isOpenForm = ref(false);
+
+const maskBinded = reactive({});
+const form = reactive({
+    name: '',
+    phone: '',
+});
+
+
+async function writeUserData(name, email, phone) {
+    try {
+        const db = getDatabase();
+        const postListRef = refFirebase(db, 'feedbackRequest');
+        const newPostRef = push(postListRef);
+        await set(newPostRef, {
+            username: name,
+            email: email,
+            phone: phone,
+            created: new Date().toLocaleString(),
+        });
+        return true
+    } catch (error) {
+        return new Error(error)
+    }
+}
+
+
+const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    result.value =  await writeUserData(form.name, 'ВОРКШОП', form.phone);
+
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push ({'event': 'formSuccessMain'});
+    
+    for (let prop in form) {
+        form[prop] = '';
+    }
+
+    props.closeModal()
+};
+</script>
+
+<style scoped></style>
